43f69acb167a50325410e8bd77aae48484959525
YalandHong <yalandhong@foxmail.com>
2019-08-06 11:42:42 +0800

    完善角色管理的异常处理

--- src/main/java/com/guotu/gt/dto/PermissionRoleDTO.java
+++ src/main/java/com/guotu/gt/dto/PermissionRoleDTO.java
@@ -6,6 +6,7 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 
 @ApiModel("角色管理界面数据实体")
@@ -19,7 +20,7 @@ public class PermissionRoleDTO {
     private Integer code;
 
     @ApiModelProperty(value = "角色名", required = true)
-    @NotNull(message = "角色名称不能为空")
+    @NotBlank(message = "角色名称不能为空，必须至少包含一个有效字符")
     private String name;
 
     @ApiModelProperty("描述")
